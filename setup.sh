1f1de3377475a6d30e5ef7ac55f8176a77e46b1c
kuro <86814204+kurox313@users.noreply.github.com>
2025-07-27 20:47:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,66 +1,55 @@
 #!/bin/bash
+
 set -e
 
-# Confirm device
-DISK="/dev/mmcblk2"
+# Configs
 HOSTNAME="arch-linux"
 USERNAME="kuro"
 PASSWORD="1111"
 LOCALE="en_US.UTF-8"
 TIMEZONE="Asia/Bangkok"
-MIRROR_COUNTRY="Singapore"
+DRIVE="/dev/mmcblk2"
 
-# Partition
-echo "Partitioning $DISK..."
-sgdisk -Z "$DISK"
-sgdisk -n1:1M:+300M -t1:ef00 -c1:EFI "$DISK"
-sgdisk -n2:0:0 -t2:8300 -c2:ROOT "$DISK"
-mkfs.fat -F32 "${DISK}p1"
-mkfs.ext4 "${DISK}p2"
+# Partitions
+EFI="${DRIVE}p1"
+ROOT="${DRIVE}p2"
 
-# Mounting
-mount "${DISK}p2" /mnt
-mkdir -p /mnt/boot/efi
-mount "${DISK}p1" /mnt/boot/efi
-
-# Mirror list
-reflector --country "$MIRROR_COUNTRY" --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-
-# Base install
-pacstrap /mnt base linux linux-firmware grub efibootmgr sudo networkmanager \
-    plasma kde-applications sddm nano zram-generator
-
-# fstab
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Chroot setup
-arch-chroot /mnt /bin/bash <<EOF
-ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+echo "==> Setting timezone and locale..."
+ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 hwclock --systohc
-sed -i 's/#$LOCALE/$LOCALE/' /etc/locale.gen
+echo "${LOCALE} UTF-8" > /etc/locale.gen
 locale-gen
-echo "LANG=$LOCALE" > /etc/locale.conf
+echo "LANG=${LOCALE}" > /etc/locale.conf
+echo "KEYMAP=us" > /etc/vconsole.conf
+
+# Hostname and network
 echo "$HOSTNAME" > /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
-
-# User setup
-echo "root:$PASSWORD" | chpasswd
-useradd -m -G wheel -s /bin/bash $USERNAME
-echo "$USERNAME:$PASSWORD" | chpasswd
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-# Enable services
-systemctl enable NetworkManager
-systemctl enable sddm
-systemctl enable zramd
-
-# Bootloader for 32-bit UEFI
-mkdir -p /boot/efi/EFI/BOOT
-cp /boot/grub/i386-efi/grub.efi /boot/efi/EFI/BOOT/BOOTIA32.EFI
-grub-install --target=i386-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+cat <<EOF > /etc/hosts
+127.0.0.1 localhost
+::1       localhost
+127.0.1.1 $HOSTNAME.localdomain $HOSTNAME
 EOF
 
-echo "Done! You can now reboot."
+echo "==> Setting root password..."
+echo "root:$PASSWORD" | chpasswd
+
+echo "==> Creating user $USERNAME..."
+useradd -m -G wheel -s /bin/bash $USERNAME
+echo "$USERNAME:$PASSWORD" | chpasswd
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+echo "==> Installing base system..."
+pacman -Syu --noconfirm grub efibootmgr sudo networkmanager linux linux-firmware \
+    base-devel vim nano network-manager-applet zramd \
+    plasma kde-applications sddm
+
+echo "==> Enabling services..."
+systemctl enable NetworkManager
+systemctl enable sddm
+systemctl enable zramd || echo "zramd not available, skipping..."
+
+echo "==> Setting up GRUB for i386-efi..."
+grub-install --target=i386-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "==> Done! Reboot to finish."
