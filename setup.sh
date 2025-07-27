789679402a84176fe4a2a52500cb2b69e0c94d53
kuro <86814204+kurox313@users.noreply.github.com>
2025-07-27 20:51:50 +0700

    Update setup.sh
    holy fixes bro

--- setup.sh
+++ setup.sh
@@ -1,55 +1,79 @@
 #!/bin/bash
-
 set -e
 
-# Configs
+# Set up variables
+DISK="/dev/mmcblk1"
 HOSTNAME="arch-linux"
 USERNAME="kuro"
 PASSWORD="1111"
 LOCALE="en_US.UTF-8"
 TIMEZONE="Asia/Bangkok"
-DRIVE="/dev/mmcblk2"
+KEYMAP="us"
+MIRROR_REGION="Singapore"
 
-# Partitions
-EFI="${DRIVE}p1"
-ROOT="${DRIVE}p2"
+# Partition the disk
+echo "[+] Partitioning $DISK..."
+sgdisk -Z $DISK
+sgdisk -n1:0:+512M -t1:ef00 -c1:EFI $DISK
+sgdisk -n2:0:0 -t2:8300 -c2:ROOT $DISK
+EFI_PART="${DISK}p1"
+ROOT_PART="${DISK}p2"
 
-echo "==> Setting timezone and locale..."
-ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+mkfs.vfat -F32 $EFI_PART
+mkfs.ext4 $ROOT_PART
+
+# Mount
+mount $ROOT_PART /mnt
+mkdir -p /mnt/boot/efi
+mount $EFI_PART /mnt/boot/efi
+
+# Set mirrors
+echo "[+] Setting mirror to Singapore..."
+reflector --country "$MIRROR_REGION" --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# Install base system
+echo "[+] Installing base system..."
+pacstrap /mnt base linux linux-firmware grub efibootmgr networkmanager sudo plasma kde-applications xorg sddm nano
+
+# Generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Chroot and configure system
+arch-chroot /mnt /bin/bash <<EOF
+
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
-echo "${LOCALE} UTF-8" > /etc/locale.gen
+
+echo "$LOCALE UTF-8" > /etc/locale.gen
 locale-gen
-echo "LANG=${LOCALE}" > /etc/locale.conf
-echo "KEYMAP=us" > /etc/vconsole.conf
+echo "LANG=$LOCALE" > /etc/locale.conf
+echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
 
-# Hostname and network
 echo "$HOSTNAME" > /etc/hostname
-cat <<EOF > /etc/hosts
-127.0.0.1 localhost
-::1       localhost
-127.0.1.1 $HOSTNAME.localdomain $HOSTNAME
-EOF
+echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
 
-echo "==> Setting root password..."
-echo "root:$PASSWORD" | chpasswd
-
-echo "==> Creating user $USERNAME..."
-useradd -m -G wheel -s /bin/bash $USERNAME
+useradd -m -G wheel $USERNAME
 echo "$USERNAME:$PASSWORD" | chpasswd
-echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo "root:$PASSWORD" | chpasswd
+echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
 
-echo "==> Installing base system..."
-pacman -Syu --noconfirm grub efibootmgr sudo networkmanager linux linux-firmware \
-    base-devel vim nano network-manager-applet zramd \
-    plasma kde-applications sddm
-
-echo "==> Enabling services..."
 systemctl enable NetworkManager
 systemctl enable sddm
-systemctl enable zramd || echo "zramd not available, skipping..."
 
-echo "==> Setting up GRUB for i386-efi..."
-grub-install --target=i386-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+# Zram swap using archinstall style (zram-generator)
+pacman -Sy --noconfirm zram-generator
+
+cat > /etc/systemd/zram-generator.conf <<ZRAMCONF
+[zram0]
+zram-size = ram
+compression-algorithm = zstd
+ZRAMCONF
+
+# Bootloader setup (32-bit UEFI)
+mkdir -p /boot/efi/EFI/BOOT
+grub-install --target=i386-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "==> Done! Reboot to finish."
+EOF
+
+echo "[+] Installation complete! You can now reboot."
